43d0a99058463768c64962b36b1bfe9c13150a4e
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2018-03-13 19:30:18 +1100

    Runner Class added.

---
+++ LorealBddDemo/src/test/java/com/mazum/MyTest.java
@@ -0,0 +1,15 @@
+package com.mazum;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:test.feature",
+        glue = "classpath:com.mazum",
+        plugin = "html:target/selenium-reports"
+)
+
+public class MyTest {
+}
