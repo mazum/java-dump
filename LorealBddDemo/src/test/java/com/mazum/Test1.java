a8d5ab8ffa8013ea41788ded621c1af6600571a5
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2018-03-14 23:59:04 +1100

    Added assert for testing.

--- LorealBddDemo/src/test/java/com/mazum/Test1.java
+++ LorealBddDemo/src/test/java/com/mazum/Test1.java
@@ -3,6 +3,8 @@ package com.mazum;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import static org.junit.Assert.assertTrue;
+
 public class Test1 {
     @When("^I am on x page$")
     public void i_am_on_x_page() throws Throwable {
@@ -11,6 +13,7 @@ public class Test1 {
 
     @Then("^I see that element$")
     public void i_see_that_element() throws Throwable {
+        assertTrue("This will succeed", true);
         System.out.println("see that element");
     }
 }
