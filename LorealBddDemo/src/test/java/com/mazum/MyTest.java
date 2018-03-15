5a063ccae7052d1cb5b91032a2dc0e877ea242ab
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2018-03-15 23:57:38 +1100

    Added first hook.

--- LorealBddDemo/src/test/java/com/mazum/MyTest.java
+++ LorealBddDemo/src/test/java/com/mazum/MyTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:test.feature",
-        glue = "classpath:com.mazum",
+        glue = {"classpath:com.mazum","com.mazum.hooks"},
         plugin = "html:target/selenium-reports"
 )
 
---
+++ LorealBddDemo/src/test/java/com/mazum/hooks/Hooks.java
@@ -0,0 +1,10 @@
+package com.mazum.hooks;
+
+import cucumber.api.java.Before;
+
+public class Hooks {
+    @Before
+    public void initiate() {
+
+    }
+}
