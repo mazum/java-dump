afc65f010ba2230bbb1635e1cc220dc2b2fd9f9f
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2018-03-17 23:59:38 +1100

    TestSettings.java and contants.properties files added.

---
+++ LorealBddDemo/src/test/java/com/mazum/utilities/TestSettings.java
@@ -0,0 +1,35 @@
+package com.mazum.utilities;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class TestSettings {
+
+    public static Properties constantsProp = new Properties();
+    public static TestSettings _instance;
+
+    public static TestSettings getInstance(){
+        if (_instance == null){
+            _instance = new TestSettings();
+        }
+        return _instance;
+    }
+
+    public void loadConstantsProperties(){
+        System.out.println("LOADING CONSTANTS PROPS");
+        try {
+            InputStream in = this.getClass().getClassLoader().getResourceAsStream("org/unisuper/properties/constants.properties");
+            if(in!=null){
+                constantsProp.load(in);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public TestSettings(){
+
+        System.out.println("LOADED CONSTANTS PROPERTIES");
+        loadConstantsProperties();
+    }
+}
