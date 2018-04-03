959cdb5069153d4992368d898f72441790f4898e
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2018-04-03 23:01:40 +1000

    Updated IEDriver creation and added FireFox Driver creation.

--- LorealBddDemo/src/test/java/com/mazum/utilities/DriverFactory.java
+++ LorealBddDemo/src/test/java/com/mazum/utilities/DriverFactory.java
@@ -1,22 +1,41 @@
 package com.mazum.utilities;
 
 import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
     public static WebDriver Driver;
     private static boolean isMobile;
 
-    public static void CreateNewDriverInstance(String browser, String gridUrl) {
+    public static void CreateNewDriverInstance(String browser, String gridUrl) throws MalformedURLException {
         DesiredCapabilities capabilities;
         switch (browser.toLowerCase()) {
             case "ie":
                 InternetExplorerOptions ieOptions = new InternetExplorerOptions();
                 ieOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+                ieOptions.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
+                ieOptions.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+                if (gridUrl.isEmpty()) {
+                    Driver = new InternetExplorerDriver(ieOptions);
+                } else {
+                    Driver = new RemoteWebDriver(new URL(gridUrl), ieOptions);
+                }
                 break;
             case "firefox":
+                if (gridUrl.isEmpty()) {
+                    Driver = new FirefoxDriver();
+                } else {
+                    Driver = new RemoteWebDriver(new URL(gridUrl), DesiredCapabilities.firefox());
+                }
                 break;
             case "chrome":
                 break;
